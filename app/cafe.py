a17a4ed1229d2e51d76cd24ccc6572f1898ab03e
fabio-Silva-E <fs2894488@gmail.com>
2026-04-13 10:54:35 -0300

    solution

--- app/cafe.py
+++ app/cafe.py
@@ -12,7 +12,7 @@ class Cafe:
     def __init__(self, name: str) -> str:
         self.name = name
 
-    def visit_cafe(self, visitor: dict) -> dict:
+    def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
             raise NotVaccinatedError("Visitor is not vaccinated")
 
